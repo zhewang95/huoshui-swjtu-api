a3335d7b4a9794d152fc75f25e53257cd037ab20
wangzhe <wangzhe140@163.com>
2017-03-01 19:52:23 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 /**
  * Created by wz on 17-3-1.
  */
-var express=require('express');
+var express = require('express');
 
-var app=express();
+var app = express();
 
-app.use(function(req,res){
-    res.statusCode=404;
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+app.use(function (req, res) {
+    res.statusCode = 404;
     res.render('404');
 });
 
-app.listen(3001,function(){
+app.listen(3001, function () {
     console.log('start listening at 3001');
 });
